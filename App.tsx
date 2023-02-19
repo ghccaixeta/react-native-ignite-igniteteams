04b987370385906a4e59c302eb0cd856fd427dd9
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2023-02-19 20:13:01 -0300

    Font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import { ActivityIndicator } from 'react-native';
 import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator/>}
 
     </ThemeProvider>
   );
