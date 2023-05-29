5b087d3ccd205be8b4555e8b36f1edb90cd8986b
ghccaixeta <gustavohcaixeta@gmail.com>
2023-05-29 08:38:15 -0300

    CSS-in-JS - Listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do Fortnite"
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Fortnite"></GroupCard>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
