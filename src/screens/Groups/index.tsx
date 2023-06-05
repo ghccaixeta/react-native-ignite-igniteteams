78445807203637f38222456e2245edb5d515a1a3
ghccaixeta <gustavohcaixeta@gmail.com>
2023-06-05 08:45:48 -0300

    CSS-in-JS - Criando Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
@@ -22,6 +23,9 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={()=><ListEmpty message="Que tal cadastrar a primeira turma?"/>}
       />
+
+      <Button title="Criar nova turma"></Button>
+
     </Container>
   );
 }
