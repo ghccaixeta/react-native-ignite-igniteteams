1cd21b7b8e90bf3a420002fa47542b4f37ae4dd4
ghccaixeta <gustavohcaixeta@gmail.com>
2023-06-21 19:20:46 -0300

    CSS-in-JS - Make icon dynamic

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,14 +1,19 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
 export function Players() {
     return (
         <Container>
             <Header showBackButton />
             <Highlight title="Nome da turma" subtitle="Adicione a galera e separe os times"/>
-            <ButtonIcon />
+            <Form>
+
+            <Input placeholder="Nome da pessoa" autoCorrect={false} />
+            <ButtonIcon icon="add"/>
+            </Form>
         </Container>
     );
 }
