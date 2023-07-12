f1f69566e793c1f5a76aade73c43929717ff2bbf
ghccaixeta <gustavohcaixeta@gmail.com>
2023-07-12 19:23:13 -0300

    Local Storage - Abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -30,6 +30,10 @@ export function Groups() {
     navigation.navigate('new');
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', {group});
+  }
+
 
   useFocusEffect(useCallback(()=>{
     fetchGroups()
@@ -42,7 +46,7 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => <GroupCard title={item} onPress={()=> handleOpenGroup(item)} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={()=><ListEmpty message="Que tal cadastrar a primeira turma?"/>}
       />
